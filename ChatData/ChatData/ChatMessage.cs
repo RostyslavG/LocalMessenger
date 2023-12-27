ec4de4026513ab6a4b2ef96a66984e5502fb0915
RostyslavG <125557213+RostyslavG@users.noreply.github.com>
2023-12-27 11:43:01 +0200

    Data

---
+++ ChatData/ChatData/ChatMessage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatData
+{
+    [Serializable]
+    public class ChatMessage
+    {
+        public String From { get; set; }
+        public String To { get; set; }
+        public String Command { get; set; }
+        public String TextMessage { get; set; }
+        public String FileName { get; set; }
+        public byte[] FileData { get; set; }
+        public DateTime CreatedAT { get; set; }
+    }
+}
